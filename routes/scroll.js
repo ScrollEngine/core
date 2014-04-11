97b10570f93b945b5b167be80bc25c8219f6c63e
Richard Key <rich@busyrich.com>
2014-04-10 21:38:54 -0700

    Made the scroll routes more configurable

--- routes/scroll.js
+++ routes/scroll.js
@@ -3,15 +3,19 @@
  * @param app - The Scroll app instance
  */
 module.exports = function(app) {
-  app.get('/post/:slug', function(req, res) {
-    this.controllers.scroll.get('post', req.params.slug, function(err, scroll) {
-      res.render('post', {post:scroll});
-    });
-  }.bind(app));
+  var config = app.config.views;
 
-  app.get('/:page', function(req, res) {
-    this.controllers.scroll.get('page', req.params.page, function(err, scroll) {
-      res.render('page', {page:scroll});
-    });
-  }.bind(app));
+  app.get('/post/:slug', (config.post.handler || function(req, res) {
+    this.controllers.scroll.get('post', req.params.slug,
+      function(err, scroll) {
+        res.render(config.post.view, {post:scroll});
+      });
+  }).bind(app));
+
+  app.get('/:page', (config.page.handler || function(req, res) {
+    this.controllers.scroll.get('page', req.params.page,
+      function(err, scroll) {
+        res.render(confg.page.view, {page:scroll});
+      });
+  }).bind(app));
 };
