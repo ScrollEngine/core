e7dd8de5668ed7958d5757509de0c2a66dac1ec2
Richard Key <rich@busyrich.com>
2014-04-10 21:15:26 -0700

    Added a page route that renders page type scrolls.

--- routes/scroll.js
+++ routes/scroll.js
@@ -8,4 +8,10 @@ module.exports = function(app) {
       res.render('post', {post:scroll});
     });
   }.bind(app));
+
+  app.get('/:page', function(req, res) {
+    this.controllers.scroll.get('page', req.params.page, function(err, scroll) {
+      res.render('page', {page:scroll});
+    });
+  }.bind(app));
 };
