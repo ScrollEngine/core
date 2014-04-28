b57e0b697944ef845cb2dc8d1598c746a7d4ec35
Richard Key <rich@busyrich.com>
2014-04-27 20:43:30 -0700

    Added the ability to load custom controllers

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -4,9 +4,24 @@
  */
 module.exports = function(app) {
   var controllers = {},
-      models = require('./data')(app);
+      models = require('./data')(app),
+      appControllers = app.util.getJSFiles(
+        app.config.__path + '/' + app.config.controller.folder
+      );
 
   controllers.scroll = require('./scroll')(models.scroll, app.parse);
+
+  var controller = null;
+  for(var c in appControllers) {
+    controller = require(appControllers[c]);
+
+    if(typeof controller === 'function') {
+      controllers[c] = controller(models[c], controllers);
+    } else if(typeof controller === 'object') {
+      controllers[controller.name] =
+        controller.load(models[controller.name], controllers);
+    }
+  }
 
   // return the loaded controllers
   return controllers;
